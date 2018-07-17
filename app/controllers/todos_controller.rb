1f0a35defe9d2e53f1539069efbf9969505501e0
Shinsuke Kido <downmap555@gmail.com>
2018-07-17 13:24:30 +0900

    index アクション、create アクションの作成

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,18 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    render json: @todo, status: :created
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title, :text)
+  end
 end
