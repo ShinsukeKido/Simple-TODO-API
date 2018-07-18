112160ae3e0f93a3b37a8f59b218cf4f86bff56c
Shinsuke Kido <downmap555@gmail.com>
2018-07-18 16:37:53 +0900

    JSON.parse(response.body) を let ではなく、変数で定義（まだ見ぬ response を 先に定義してしまうことに違和感があるため）

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe TodosController, type: :request do
   describe '#index' do
-    let(:jsons) { JSON.parse(response.body) }
-
     before { 3.times { create(:todo) } }
 
     it 'HTTP ステータスコード 200 が返る' do
@@ -13,20 +11,23 @@ RSpec.describe TodosController, type: :request do
 
     it 'todo が作成された数だけ JSON 形式で返る' do
       get '/todos'
+      jsons = JSON.parse(response.body)
       expect(jsons.count).to eq 3
     end
 
     it '出力される JSON のキーが仕様通りである' do
       get '/todos'
-      expect(jsons[0].keys).to eq %w[id title text created_at]
+      jsons = JSON.parse(response.body)
     end
 
     it '返す JSON に、作成した todo の内容が正しく反映されている' do
       get '/todos'
-      expect(jsons[0]['id']).to eq Todo.order(:created_at).first.id
-      expect(jsons[0]['title']).to eq Todo.order(:created_at).first.title
-      expect(jsons[0]['text']).to eq Todo.order(:created_at).first.text
-      expect(jsons[0]['created_at']).to eq Todo.order(:created_at).first.created_at.as_json
+      jsons = JSON.parse(response.body)
+      todo = Todo.order(:created_at).first
+      expect(jsons[0]['id']).to eq todo.id
+      expect(jsons[0]['title']).to eq todo.title
+      expect(jsons[0]['text']).to eq todo.text
+      expect(jsons[0]['created_at']).to eq todo.created_at.as_json
     end
   end
 end
