768e14816e49c52ccca14ea925a831be3e0d6375
Shinsuke Kido <downmap555@gmail.com>
2018-07-25 16:13:52 +0900

    JSON で返させる値のテストを、ハッシュの完全一致のテストから、各キーに対するバリューの合致をテストする形式に、分割

--- spec/serializers/todo_serializer_spec.rb
+++ spec/serializers/todo_serializer_spec.rb
@@ -1,19 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe TodoSerializer, type: :serializer do
+  let(:json) { serializer.attributes }
   let(:serializer) { described_class.new(todo) }
   let(:todo) { create(:todo) }
 
-  let(:expected_response) do
-    {
-      id: todo.id,
-      title: todo.title,
-      text: todo.text,
-      created_at: todo.created_at.as_json,
-    }
+  it '返す JSON の id に、作成した todo の id が正しく反映されている' do
+    expect(json[:id]).to eq todo.id
   end
 
-  it '返す JSON に、作成した todo の内容が正しく反映されている' do
-    expect(serializer.to_json).to be_json_as expected_response
+  it '返す JSON の title に、作成した todo の title が正しく反映されている' do
+    expect(json[:title]).to eq todo.title
+  end
+
+  it '返す JSON の text に、作成した todo の text が正しく反映されている' do
+    expect(json[:text]).to eq todo.text
+  end
+
+  it '返す JSON の created_at に、作成した todo の created_at が正しく反映されている' do
+    expect(json[:created_at]).to eq todo.created_at
+  end
+
+  it '返す JSON のキーが、仕様通りであること' do
+    expect(json.keys).to contain_exactly(:id, :title, :text, :created_at)
   end
 end
