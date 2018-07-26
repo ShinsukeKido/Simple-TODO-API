519c4e54c7feed226b1bf3add950d12d82f0a067
Shinsuke Kido <downmap555@gmail.com>
2018-07-26 10:53:28 +0900

    let の引数名を変更
    json よりも attributes の方が、実態に伴っているため

--- spec/serializers/todo_serializer_spec.rb
+++ spec/serializers/todo_serializer_spec.rb
@@ -1,27 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe TodoSerializer, type: :serializer do
-  let(:json) { serializer.attributes }
-  let(:serializer) { described_class.new(todo) }
+  let(:attributes) { described_class.new(todo).attributes }
   let(:todo) { create(:todo) }
 
   it '返す JSON の id に、作成した todo の id が正しく反映されている' do
-    expect(json[:id]).to eq todo.id
+    expect(attributes[:id]).to eq todo.id
   end
 
   it '返す JSON の title に、作成した todo の title が正しく反映されている' do
-    expect(json[:title]).to eq todo.title
+    expect(attributes[:title]).to eq todo.title
   end
 
   it '返す JSON の text に、作成した todo の text が正しく反映されている' do
-    expect(json[:text]).to eq todo.text
+    expect(attributes[:text]).to eq todo.text
   end
 
   it '返す JSON の created_at に、作成した todo の created_at が正しく反映されている' do
-    expect(json[:created_at]).to eq todo.created_at
+    expect(attributes[:created_at]).to eq todo.created_at
   end
 
   it '返す JSON のキーが、仕様通りであること' do
-    expect(json.keys).to contain_exactly(:id, :title, :text, :created_at)
+    expect(attributes.keys).to contain_exactly(:id, :title, :text, :created_at)
   end
 end
